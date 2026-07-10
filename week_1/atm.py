c9e250883ed53971ec3a9ea3c12b9354ca405899
Kushal Thapa <kushalthapa@Kushals-MacBook-Air.local>
2026-07-10 17:09:50 +0545

    Fix: trekking scenario logic & commenting

--- week_1/atm.py
+++ week_1/atm.py
@@ -1,18 +1,27 @@
-pin_user = 9999
-flag = 0
+# classwork: simulate ATM machine
 
-username = input("Enter the username: ")
+pin_user = 9999  # hardooding the pin
+flag = 0  # user trials
 
+username = input("Enter the username: ")  # username input
+
+# user has only three attempts to enter the correct pin in total
 for i in range(3):
+    # taking pin as the input
     pin_input = int(input("Enter the pin: "))
-    
-    if(pin_input==pin_user):
-        print("Enter the amount")
+
+    # checking the correctness of the pin
+    if pin_input == pin_user:
+        # user is able to take amount once they enter correct pin
+        amount = float(input("Enter the amount to withdraw: "))
+        print(f"Withdrawing {amount} from your account.")
+        # user attempt is successful
         flag += 1
         break
     else:
+        # user attempt is unsuccessful
         print("pin verification failed, try again")
 
-if(flag == 0):
+if flag == 0:
+    # if user fails to enter the correct pin in 3 attempts
     print("Pin didn't match for 3 attempts")
-
--- week_1/trek.py
+++ week_1/trek.py
@@ -1,17 +1,28 @@
 # Trekking Permit Cost Calculator
 
-person_1 = float(input("Enter First person's contribution amount: "))
-person_2 = float(input("Enter Second person's contribution amount: "))
+# taking the number of people as input
+n = int(input("Enter number of people in the group: "))
 
-group_expense = person_1 + person_2
+contributions = []  # list to store contibution amount of each person
 
+# loop to take contibutions from each person
+for i in range(n):
+    contribution = float(input(f"Enter person {i + 1}'s TIMS + ACAP fees: "))
+    contributions.append(contribution)
+
+# calculating the group expense
+group_expense = sum(contributions)
+
+# calculating the service charge
 service_charge = group_expense * 0.05
 
+# expense including the charge
 total_cost = group_expense + service_charge
 
-avg_cost = total_cost/2
+# average cost per person incldung the charge
+avg_cost = total_cost / n
 
+# prining the results
+print(f"Group expense: {group_expense}")
 print(f"Total cost of the group: {total_cost}")
 print(f"Average cost of the group: {avg_cost}")
-
-
