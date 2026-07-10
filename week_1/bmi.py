1db1cf939599ac79dde5d19aecc69dc9fbce2f02
Kushal Thapa <kushalthapa@Kushals-MacBook-Air.local>
2026-07-10 17:28:52 +0545

    Add comments

--- week_1/bmi.py
+++ week_1/bmi.py
@@ -1,11 +1,14 @@
 # BMI for a Community Health Camp
 
+# taking weight and height as input
 weight = float(input("Enter weight in kgs: "))
 height = float(input("Enter height in cms: "))
 
+# converting the height from cms to metres
 height_metres = height / 100
 
-bmi = weight / (height_metres ** 2)
+# BMI = weight (in kg) / (height (in m))^2
+bmi = weight / (height_metres**2)
 
+# calculate the BMI
 print(f"BMI Index: {bmi:.1f}")
-
