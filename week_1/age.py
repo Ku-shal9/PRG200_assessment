ad8dca1e4528343c44a99111b0910652e8ec160f
Kushal Thapa <kushalthapa@Kushals-MacBook-Air.local>
2026-07-10 17:34:07 +0545

    Add comments

--- week_1/age.py
+++ week_1/age.py
@@ -1,7 +1,11 @@
 # Age Calculator (BS Birth Year)
 
+# taking current and birth year as input
 birth_year = int(input("Enter your birth year (BS): "))
-current_year = int(input("Enter the current BS year: "))  
-age = current_year - birth_year
-print(f"Your age is: {age} years")
+current_year = int(input("Enter the current BS year: "))
 
+# age is difference between current year and birth year
+age = current_year - birth_year
+
+# printing the age
+print(f"Your age is: {age} years")
