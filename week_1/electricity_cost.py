dfe0a299106ba849bf4a0345f020d0b9393f3a33
Kushal Thapa <kushalthapa@Kushals-MacBook-Air.local>
2026-07-10 17:16:44 +0545

    Add: commit message

--- week_1/electricity_cost.py
+++ week_1/electricity_cost.py
@@ -1,12 +1,15 @@
 # NEA Electricity Unit Cost
 
+# taking previous and current meter readings
 prev_reading = float(input("Enter the previous meter readings: "))
 curr_reading = float(input("Enter the current meter readings: "))
 
+# defining the flat per unit rate and service charge
 flat_per_unit_rate = 15
 service_charge = 20
 
-total_bill = ((curr_reading-prev_reading)*flat_per_unit_rate) + service_charge
+# calculating the total bill
+total_bill = ((curr_reading - prev_reading) * flat_per_unit_rate) + service_charge
 
+# printing total bill
 print(f"Total bill: {total_bill}")
-
