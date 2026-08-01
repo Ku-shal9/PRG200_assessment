cbac6ea33d7a910bfcb522721b9713489e1d7c3b
Kushal Thapa <k.thapa.5945@westcliff.edu>
2026-08-01 18:19:42 +0545

    commenting

--- sajha_yatayat.py
+++ sajha_yatayat.py
@@ -1,22 +1,30 @@
+# Bus Ticket Booking (Sajha Yatayat)
+# class of Bus
 class Bus:
     def __init__(self, route, total_seats):
         self.route = route
         self.total_seats = total_seats
-        self.booked = []
+        self.booked = []  # list for booking status
 
+    # function to book a seat
     def book_seat(self, seat_number, passenger_name):
+        # checking if the seat is already booked
         for seat, passenger in self.booked:
+            # if the applied seat number and
+            # book number in the list is same then seat is not available
             if seat == seat_number:
                 print("Oops! Seat is not available")
                 return
-
+        # if the seat is available then booking is done
         self.booked.append((seat_number, passenger_name))
         print(f"Seat {seat_number} booked for {passenger_name}")
 
+    # function to check available seats
     def available_seats(self):
         available_seats = self.total_seats - len(self.booked)
         return available_seats
 
+    # function to display passenger list
     def passenger_list(self):
         print("\nPassenger List:")
 
@@ -24,6 +32,7 @@ class Bus:
             print(f"Seat {seat}: {passenger}")
 
 
+# provided data
 bus = Bus("Kathmandu - Pokhara", 10)
 
 bookings = [
@@ -33,7 +42,7 @@ bookings = [
     (1, "Prakash Magar"),
     (7, "Suman Tamang"),  # Duplicate
 ]
-
+# going through the bookings and booking the seats
 for seat, passenger in bookings:
     bus.book_seat(seat, passenger)
 
--- student_report.py
+++ student_report.py
@@ -1,18 +1,20 @@
 # Student Report Card
 
-
+# student class
 class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
 
+    # calculate average marks
     def average(self):
         average_mark = sum(self.marks) / len(self.marks)
         return average_mark
 
+    # calculate grade based on average marks
     def grade(self):
         avg = self.average()
-
+        # various condition for grading
         if avg >= 80:
             return "A"
         elif avg >= 65:
@@ -24,16 +26,17 @@ class Student:
         else:
             return "F"
 
+    # function to display grades
     def display(self):
         avg = self.average()
         grade = self.grade()
         status = ""
-
+        # pass/ fail status based on average marks
         if avg >= 40:
             status = "Pass"
         else:
             status = "Fail"
-
+        # printing student report card
         print(f"""
         Name: {self.name}
         Average: {avg}
@@ -42,17 +45,19 @@ class Student:
               """)
 
 
+# provided data
 students = [
     ("Aarav", [78, 85, 60, 90, 72]),
     ("Sita", [45, 50, 38, 60, 55]),
     ("Bishal", [30, 25, 40, 35, 28]),
     ("Priya", [90, 88, 95, 92, 87]),
 ]
-
+# list of student report cards object
 student_report = []
-
+# going through the lis
 for name, marks in students:
+    # creating student object and appending to the list
     student_report.append(Student(name, marks))
-
+# displaying student report cards
 for student in student_report:
     student.display()
